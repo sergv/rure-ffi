00a8b6cb43713601d3c499bc61e7995b50f18830
Sergey Vinokurov <serg.foo@gmail.com>
2025-10-04 00:27:56 +0100

    Initial raw bindings

---
+++ src/Data/Regex/Rure.hs
@@ -0,0 +1,8 @@
+-- |
+-- Module:     Data.Regex.Rure
+-- Copyright:  (c) Sergey Vinokurov 2025
+-- License:    Apache-2.0 (see LICENSE)
+-- Maintainer: serg.foo@gmail.com
+
+module Data.Regex.Rure where
+
